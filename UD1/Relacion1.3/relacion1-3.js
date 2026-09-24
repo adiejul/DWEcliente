4f711544df07eb2abfbcf6395b586450cb479f5b
Ana Dieguez <anadieguez010809@gmail.com>
2026-09-24 16:00:00 +0200

    ejercicio 2 completado

--- UD1/Relacion1.3/relacion1-3.js
+++ UD1/Relacion1.3/relacion1-3.js
@@ -4,6 +4,19 @@ function ageCalculator(edad){
 
     let res = anyoActual - edad;
 
-    return "Tienen " + (res-1) + " o " + res + " años";
+    return "Tienes " + (res-1) + " o " + res + " años";
 }
+
+function supplyCalculator(){
+
+    let edad = parseInt(prompt("Introduce tu edad: "))
+    let edadMaxima = parseInt(prompt("Introduce la edad maxima: "));
+    let cantidadDiaria = parseInt(prompt("Introduce la cantidad maxima de suministros al dia: "))
+
+    let res = ((edadMaxima - edad)*365) * cantidadDiaria  
+
+    return "Necesitaras " + res + " para que te duren hasta la avanzada edad de " + edadMaxima + " años";
+
+}
+
 
